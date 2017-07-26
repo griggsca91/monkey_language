e5583f6bcaf88dbd1c1ef1bc8a2b979e87cc615c
Chris Griggs <griggsca91@gmail.com>
2017-07-26 15:37:04 -0400

    added errors to parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -44,6 +44,21 @@ let foobar = 838383;
 	}
 }
 
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+
+	t.FailNow()
+}
+
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
